89f33f71e97dbdcb5a96687d693c66550cfc21ec
Erik Robles <erikjames69@hotmail.com>
2018-05-19 15:18:09 -0500

    adding windows object files under js5 folder

---
+++ js5/WindowObjects/window.js
@@ -0,0 +1,66 @@
+// Window - methods - objects - properties
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
+
+let val;
+
+
+// Outer height and width
+
+val = window.outerWidth;
+val = window.outerHeight;
+val = window.innerWidth;
+val = window.innerHeight;
+
+// Scroll points
+
+val = window.scrollY;
+val = window.scrollX;
+
+// Location object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// redirect
+
+// window.location.href = 'http://google.com';
+// window.locations.reload();
+
+// History Object
+
+// window.history.go(-1);
+
+// val = window.history.length;
+
+
+// Navigator Object
+
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+
+
+
+
+
+
+
+
+console.log(val);
